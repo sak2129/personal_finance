fe1339d24bdf7825dfe2a1efa9608b642ef646e9
sak2129 <sak2129@gmail.com>
2021-05-29 04:14:07 +0000

    adding login functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Main program file - this file renders the mainpage and connects all routes
 
 # Import modules
-from flask import Flask, g, redirect, render_template, request, session, url_for
+from flask import Flask, g, redirect, render_template, request, session, url_for, flash
 import pandas
 from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,Range1d)
 from bokeh.models.glyphs import VBar
@@ -26,40 +26,64 @@ users.append(User(id=2, username='Becca', password='secret'))
 users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 
+######################
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
 
-@app.before_request
-def before_request():
-    g.user = None
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/sak2129/personal_finance/login.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
-        
+db=SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    message=None
     if request.method == 'POST':
-        session.pop('user_id', None)
-        #session.permanent = False
         username = request.form['username']
         password = request.form['password']
-        
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('home'))
 
-        return redirect(url_for('login'))
+        if username=='sak2129' and password=='chikki':
+            user = User.query.filter_by(username=username).first()
+            login_user(user)
+            return render_template('home.html', message=message)
+        else:
+            flash('Incorrect credentials - please try again.')
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if current_user.is_authenticated:
+        return render_template('home.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Successfully logged out.')
     return render_template('login.html')
 
-@app.route('/')
-def home():
-    if not g.user:
-        return redirect(url_for('login'))
-    else:
-        return render_template('home.html')
+@app.route('/currentuser')
+@login_required
+def currentuser():
+    return 'The current user is ' + current_user.username 
 
 @app.route('/homepage')
 def homepage():
@@ -97,8 +121,8 @@ dataframe = (bqclient.query(query_string).result().to_dataframe(bqstorage_client
 actual_data = dataframe[dataframe['actual_expense'].notnull()][['timestamp','actual_expense']]
 forecast_data = dataframe[dataframe['forecast_value'].notnull()][['timestamp','forecast_value']]
 
-@app.route('/historical')
-def historical():
+@app.route('/machinelearning')
+def machinelearning():
     # Create a plot comparing the different sort algorithms
     p = figure(plot_width=950, 
             plot_height=400,
@@ -115,9 +139,9 @@ def historical():
     p.legend.location = "top_left"
     script, div = components(p)
 
-    return render_template('historical.html', actual_data=actual_data,forecast_data=forecast_data, the_div=div,the_script=script)
+    return render_template('machinelearning.html', actual_data=actual_data,forecast_data=forecast_data, the_div=div,the_script=script)
 
 
 if __name__ == '__main__':
     #app.secret_key = os.urandom(12)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=False)
