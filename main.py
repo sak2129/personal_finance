afeb4580e8df653b9192b25e37aa0b3a62e69092
sak2129 <sak2129@gmail.com>
2021-05-30 03:57:12 +0000

    cleaning up files

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ def machinelearning():
 
 if __name__ == '__main__':
     #app.secret_key = os.urandom(12)
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(port=8080, debug=False)
